3c89b86634918d813a534bd62d53f598b6f6523c
Akhilesh Halageri <akhileshhalageri@gmail.com>
2019-06-24 13:27:56 -0400

    segmentation endpoints

--- pcgserver/app/cg_app_blueprint.py
+++ pcgserver/app/cg_app_blueprint.py
@@ -14,6 +14,7 @@ import threading
 from pcgserver.app import app_utils, meshing_app_blueprint
 from pychunkedgraph.backend import chunkedgraph_exceptions as cg_exceptions, \
     chunkedgraph_comp as cg_comp
+from pychunkedgraph.api import segmentation
 from middle_auth_client import auth_required, auth_requires_roles
 
 __version__ = 'fafb.1.2'
@@ -151,7 +152,6 @@ def handle_info(table_id):
 
     return jsonify(cg.dataset_info)
 
-### GET ROOT -------------------------------------------------------------------
 
 @bp.route('/1.0/<table_id>/graph/root', methods=['POST', 'GET'])
 def handle_root_1(table_id):
@@ -185,15 +185,12 @@ def handle_root_2(table_id, atomic_id):
 def handle_root_main(table_id, atomic_id, timestamp):
     current_app.request_type = "root"
 
-    # Call ChunkedGraph
     cg = app_utils.get_cg(table_id)
     root_id = cg.get_root(np.uint64(atomic_id), time_stamp=timestamp)
 
-    # Return binary
+    return str(root_id)
     return app_utils.tobinary(root_id)
 
-
-### MERGE ----------------------------------------------------------------------
 
 @bp.route('/1.0/<table_id>/graph/merge', methods=['POST', 'GET'])
 @auth_requires_roles('edit_all')
@@ -206,81 +203,11 @@ def handle_merge(table_id):
     current_app.logger.debug(nodes)
     assert len(nodes) == 2
 
-    # Call ChunkedGraph
     cg = app_utils.get_cg(table_id)
+    new_root = segmentation.merge(cg, nodes, user_id)
 
-    atomic_edge = []
-    coords = []
-    for node in nodes:
-        node_id = node[0]
-        x, y, z = node[1:]
-
-        x /= 2
-        y /= 2
-
-        coordinate = np.array([x, y, z])
-
-        if not cg.is_in_bounds(coordinate):
-            coordinate /= cg.segmentation_resolution
-
-            coordinate[0] *= 2
-            coordinate[1] *= 2
-
-        atomic_id = cg.get_atomic_id_from_coord(coordinate[0],
-                                                coordinate[1],
-                                                coordinate[2],
-                                                parent_id=np.uint64(node_id))
-
-        if atomic_id is None:
-            raise cg_exceptions.BadRequest(
-                f"Could not determine supervoxel ID for coordinates "
-                f"{coordinate}."
-            )
-
-        coords.append(coordinate)
-        atomic_edge.append(atomic_id)
-
-    # Protection from long range mergers
-    chunk_coord_delta = cg.get_chunk_coordinates(atomic_edge[0]) - \
-                        cg.get_chunk_coordinates(atomic_edge[1])
-
-    if np.any(np.abs(chunk_coord_delta) > 3):
-        raise cg_exceptions.BadRequest(
-            "Chebyshev distance between merge points exceeded allowed maximum "
-            "(3 chunks).")
-
-    lvl2_nodes = []
-
-    try:
-        ret = cg.add_edges(user_id=user_id,
-                           atomic_edges=np.array(atomic_edge,
-                                                 dtype=np.uint64),
-                           source_coord=coords[:1],
-                           sink_coord=coords[1:],
-                           return_new_lvl2_nodes=True)
-
-        new_root, lvl2_nodes = ret
-
-    except cg_exceptions.LockingError as e:
-        raise cg_exceptions.InternalServerError(
-            "Could not acquire root lock for merge operation.")
-    except cg_exceptions.PreconditionError as e:
-        raise cg_exceptions.BadRequest(str(e))
-
-    if new_root is None:
-        raise cg_exceptions.InternalServerError(
-            "Could not merge selected supervoxel.")
-
-    if len(lvl2_nodes) > 0:
-        t = threading.Thread(target=meshing_app_blueprint._mesh_lvl2_nodes,
-                             args=(cg.get_serialized_info(), lvl2_nodes))
-        t.start()
-
-    # Return binary
     return app_utils.tobinary(new_root)
 
-
-### SPLIT ----------------------------------------------------------------------
 
 @bp.route('/1.0/<table_id>/graph/split', methods=['POST', 'GET'])
 @auth_requires_roles('edit_all')
@@ -294,79 +221,10 @@ def handle_split(table_id):
 
     # Call ChunkedGraph
     cg = app_utils.get_cg(table_id)
+    new_roots = segmentation.split(cg, data, user_id)
 
-    data_dict = {}
-    for k in ["sources", "sinks"]:
-        data_dict[k] = collections.defaultdict(list)
-
-        for node in data[k]:
-            node_id = node[0]
-            x, y, z = node[1:]
-
-            x /= 2
-            y /= 2
-
-            coordinate = np.array([x, y, z])
-
-            current_app.logger.debug(("before", coordinate))
-
-            if not cg.is_in_bounds(coordinate):
-                coordinate /= cg.segmentation_resolution
-
-                coordinate[0] *= 2
-                coordinate[1] *= 2
-
-            current_app.logger.debug(("after", coordinate))
-
-            atomic_id = cg.get_atomic_id_from_coord(coordinate[0],
-                                                    coordinate[1],
-                                                    coordinate[2],
-                                                    parent_id=np.uint64(
-                                                        node_id))
-
-            if atomic_id is None:
-                raise cg_exceptions.BadRequest(
-                    f"Could not determine supervoxel ID for coordinates "
-                    f"{coordinate}.")
-
-            data_dict[k]["id"].append(atomic_id)
-            data_dict[k]["coord"].append(coordinate)
-
-    current_app.logger.debug(data_dict)
-
-    lvl2_nodes = []
-    try:
-        ret = cg.remove_edges(user_id=user_id,
-                              source_ids=data_dict["sources"]["id"],
-                              sink_ids=data_dict["sinks"]["id"],
-                              source_coords=data_dict["sources"]["coord"],
-                              sink_coords=data_dict["sinks"]["coord"],
-                              mincut=True,
-                              return_new_lvl2_nodes=True)
-
-        new_roots, lvl2_nodes = ret
-
-    except cg_exceptions.LockingError as e:
-        raise cg_exceptions.InternalServerError(
-            "Could not acquire root lock for split operation.")
-    except cg_exceptions.PreconditionError as e:
-        raise cg_exceptions.BadRequest(str(e))
-
-    if new_roots is None:
-        raise cg_exceptions.InternalServerError(
-            "Could not split selected segment groups."
-        )
-
-    current_app.logger.debug(("after split:", new_roots))
-
-    if len(lvl2_nodes) > 0:
-        t = threading.Thread(target=meshing_app_blueprint._mesh_lvl2_nodes,
-                             args=(cg.get_serialized_info(), lvl2_nodes))
-        t.start()
-    # Return binary
     return app_utils.tobinary(new_roots)
 
-### CHILDREN -------------------------------------------------------------------
 
 @bp.route('/1.0/<table_id>/segment/<parent_id>/children',
           methods=['POST', 'GET'])
@@ -376,107 +234,44 @@ def handle_children(table_id, parent_id):
     cg = app_utils.get_cg(table_id)
 
     parent_id = np.uint64(parent_id)
-    layer = cg.get_chunk_layer(parent_id)
-
-    if layer > 1:
-        children = cg.get_children(parent_id)
-    else:
-        children = np.array([])
-
-    # Return binary
+    children = segmentation.get_children(cg, parent_id)
+    
     return app_utils.tobinary(children)
 
 
-### LEAVES ---------------------------------------------------------------------
-
 @bp.route('/1.0/<table_id>/segment/<root_id>/leaves', methods=['POST', 'GET'])
 def handle_leaves(table_id, root_id):
-    current_app.request_type = "leaves"
+    current_app.request_type = 'leaves'
 
-    if "bounds" in request.args:
-        bounds = request.args["bounds"]
-        bounding_box = np.array([b.split("-") for b in bounds.split("_")],
-                                dtype=np.int).T
-    else:
-        bounding_box = None
+    bounds = request.args.get('bounds', None)
 
-    # Call ChunkedGraph
     cg = app_utils.get_cg(table_id)
-    atomic_ids = cg.get_subgraph_nodes(int(root_id),
-                                       bounding_box=bounding_box,
-                                       bb_is_coordinate=True)
+    atomic_ids = segmentation.get_leaf_nodes(cg, root_id, bounds)
 
-    # Return binary
     return app_utils.tobinary(atomic_ids)
 
 
-### LEAVES FROM LEAVES ---------------------------------------------------------
-
-@bp.route('/1.0/<table_id>/segment/<atomic_id>/leaves_from_leave',
-          methods=['POST', 'GET'])
-def handle_leaves_from_leave(table_id, atomic_id):
-    current_app.request_type = "leaves_from_leave"
-
-    if "bounds" in request.args:
-        bounds = request.args["bounds"]
-        bounding_box = np.array([b.split("-") for b in bounds.split("_")],
-                                dtype=np.int).T
-    else:
-        bounding_box = None
-
-    # Call ChunkedGraph
-    cg = app_utils.get_cg(table_id)
-    root_id = cg.get_root(int(atomic_id))
-
-    atomic_ids = cg.get_subgraph_nodes(root_id,
-                                       bounding_box=bounding_box,
-                                       bb_is_coordinate=True)
-    # Return binary
-    return app_utils.tobinary(np.concatenate([np.array([root_id]), atomic_ids]))
-
-
-### SUBGRAPH -------------------------------------------------------------------
-
 @bp.route('/1.0/<table_id>/segment/<root_id>/subgraph', methods=['POST', 'GET'])
 def handle_subgraph(table_id, root_id):
-    current_app.request_type = "subgraph"
+    current_app.request_type = 'subgraph'
 
-    if "bounds" in request.args:
-        bounds = request.args["bounds"]
-        bounding_box = np.array([b.split("-") for b in bounds.split("_")],
-                                dtype=np.int).T
-    else:
-        bounding_box = None
+    bounds = request.args.get('bounds', None)
 
-    # Call ChunkedGraph
     cg = app_utils.get_cg(table_id)
-    atomic_edges = cg.get_subgraph_edges(int(root_id),
-                                         bounding_box=bounding_box,
-                                         bb_is_coordinate=True)[0]
-    # Return binary
-    return app_utils.tobinary(atomic_edges)
+    atomic_edges = segmentation.get_atomic_edges(cg, root_id, bounds)
+
+    return app_utils.tobinary(atomic_ids)    
 
 
-### CHANGE LOG -----------------------------------------------------------------
-
-@bp.route('/1.0/<table_id>/segment/<root_id>/change_log',
-          methods=["POST", "GET"])
+@bp.route('/1.0/<table_id>/segment/<root_id>/change_log', methods=['POST', 'GET'])
 def change_log(table_id, root_id):
-    current_app.request_type = "change_log"
+    current_app.request_type = 'change_log'
 
-    try:
-        time_stamp_past = float(request.args.get('timestamp', 0))
-        time_stamp_past = datetime.fromtimestamp(time_stamp_past, UTC)
-    except (TypeError, ValueError) as e:
-        raise(cg_exceptions.BadRequest("Timestamp parameter is not a valid"
-                                       " unix timestamp"))
-
-    # Call ChunkedGraph
     cg = app_utils.get_cg(table_id)
 
-    change_log = cg.get_change_log(root_id=np.uint64(root_id),
-                                   correct_for_wrong_coord_type=True,
-                                   time_stamp_past=time_stamp_past)
+    change_log = segmentation.get_change_log(
+        cg, root_id,
+        float(request.args.get('timestamp', 0)))
 
     return jsonify(change_log)
 
@@ -484,45 +279,29 @@ def change_log(table_id, root_id):
 @bp.route('/1.0/<table_id>/segment/<root_id>/merge_log',
           methods=["POST", "GET"])
 def merge_log(table_id, root_id):
-    current_app.request_type = "merge_log"
+    current_app.request_type = 'merge_log'
 
-    try:
-        time_stamp_past = float(request.args.get('timestamp', 0))
-        time_stamp_past = datetime.fromtimestamp(time_stamp_past, UTC)
-    except (TypeError, ValueError) as e:
-        raise(cg_exceptions.BadRequest("Timestamp parameter is not a valid"
-                                       " unix timestamp"))
-
-    # Call ChunkedGraph
     cg = app_utils.get_cg(table_id)
 
-    change_log = cg.get_change_log(root_id=np.uint64(root_id),
-                                   correct_for_wrong_coord_type=True,
-                                   time_stamp_past=time_stamp_past)
+    merge_log = segmentation.get_merge_log(
+        cg, root_id,
+        float(request.args.get('timestamp', 0)))
 
-    for k in list(change_log.keys()):
-        if not "merge" in k:
-            del change_log[k]
-            continue
+    return jsonify(merge_log)
 
-    return jsonify(change_log)
-
-
-### CONTACT SITES --------------------------------------------------------------
 
 @bp.route('/1.0/<table_id>/segment/<root_id>/contact_sites',
           methods=["POST", "GET"])
 def handle_contact_sites(table_id, root_id):
-    partners = request.args.get('partners', False)
+    partners = request.args.get('partners', False)  
 
-    if "bounds" in request.args:
-        bounds = request.args["bounds"]
-        bounding_box = np.array([b.split("-") for b in bounds.split("_")],
-                                dtype=np.int).T
-    else:
-        bounding_box = None
+    bounding_box = None
+    if 'bounds' in request.args:
+        bounds = request.args['bounds']
+        bounding_box = np.array(
+            [b.split('-') for b in bounds.split('_')], dtype=np.int).T
+        
 
-    # Call ChunkedGraph
     cg = app_utils.get_cg(table_id)
 
     cs_dict = cg_comp.get_contact_sites(cg, np.uint64(root_id),
